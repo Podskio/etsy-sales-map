01246b7f5dbb381c44561c04e76b8964f7a505d6
Podskio <podskio1@gmail.com>
2023-01-30 19:58:28 -0800

    Choropleth-style coloring

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,6 +4,21 @@ import { GeoJSON, MapContainer, TileLayer } from "react-leaflet";
 
 import states from "../data/states.json";
 
+const getColor = (numOrders, totalOrders) => {
+  if (!numOrders || numOrders == 0) return "#000"; //"#1e3a8a";
+
+  const percent = (numOrders / totalOrders) * 100;
+  if (percent < 0.5) return "#1e40af";
+  else if (percent < 1) return "#1d4ed8";
+  else if (percent < 2) return "#2563eb";
+  else if (percent < 5) return "#3b82f6";
+  else if (percent < 10) return "#60a5fa";
+  else if (percent < 12) return "#93c5fd";
+  else if (percent < 15) return "#bfdbfe";
+  else if (percent < 20) return "#dbeafe";
+  else return "#eff6ff";
+};
+
 const Map = ({ data }) => {
   return (
     <MapContainer zoom={3} center={[50, -100]} style={{ height: "100vh" }}>
@@ -16,19 +31,25 @@ const Map = ({ data }) => {
           data={states}
           style={(feature) => {
             if (!data) return;
-            const numOrders = data.get(feature.properties.NAME);
+
+            const numOrders = data.states.get(feature.properties.NAME);
+
             return {
-              color: numOrders > 0 ? "#2563eb" : "#db2777",
-              weight: 1,
+              fillColor: getColor(numOrders, data.totalOrders),
+              weight: 2,
+              color: "#1e3a8a",
+              fillOpacity: 0.7,
             };
           }}
           onEachFeature={(feature, layer) => {
             if (!data) return;
-            const numOrders = data.get(feature.properties.NAME);
+
+            const numOrders = data.states.get(feature.properties.NAME);
+
             layer.bindPopup(
-              `<h1>${feature.properties.NAME} - ${
-                numOrders ? numOrders : 0
-              } order${numOrders !== 1 ? "s" : ""}</h1>`
+              `<h1>${feature.properties.NAME} - ${numOrders ?? 0} order${
+                numOrders !== 1 ? "s" : ""
+              } - ${((numOrders / data.totalOrders) * 100).toFixed(1)}%</h1>`
             );
           }}
         />
